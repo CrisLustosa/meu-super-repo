45974169276b0b7dbf25b31c979d83e11a726df2
Cris Lustosa <cmlustosa@gmail.com>
2021-04-07 13:08:30 -0300

    Atualiza arquivo de exercicio

---
+++ exercises/5.2/script.js
@@ -0,0 +1,5 @@
+document.querySelector('#elementoOndeVoceEsta');
+document.querySelector('#elementOndeVoceEsta').parentElement.style.color = 'red';
+document.querySelector('#filhoDoFilho').innerHTML = 'Texto Adicionado';
+document.querySelector('#pai').firstElementChild;
+document.querySelector('#elementoOndeVoceEsta').previousElementSibling;
