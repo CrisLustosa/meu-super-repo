2bc4b751f9bbb8448abdbfbb5b6d909a576083f9
Cris Lustosa <cmlustosa@gmail.com>
2021-08-23 11:06:00 -0300

    Corrigindo bug do hw do computador -commit mágico

---
+++ exercises/4.4/4.4.js
@@ -0,0 +1,11 @@
+
+
+ 
+let player = {
+    name:  'Marta',
+    lastName: 'Silva',
+    age: 34,
+    
+};
+
+console.log('A jogadora ' + player.name + ' ' +  player.lastName  + ' tem ' + player.age + ' anos de idade.');
